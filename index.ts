8a78b64438d9ca65e1be355cf92fbfc3d892a5d1
Remco Haszing <remcohaszing@gmail.com>
2023-09-19 13:37:26 +0200

    Update dependencies
    This updates to unified 11 and vfile-location 5.

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
 import { generateDifferences, showInvisibles } from 'prettier-linter-helpers'
-import { Plugin } from 'unified'
+import { type Plugin } from 'unified'
 import { location } from 'vfile-location'
 
 const source = 'unified-consistency'
 const url = `https://github.com/remcohaszing/${source}`
-const del = `${source}:delete`
-const insert = `${source}:insert`
-const replace = `${source}:replace`
 
 /**
  * A unified plugin which reports inconsistencies between the input and what a pipeline would
  * output.
  */
-const unifiedConsistency: Plugin<[]> = function unifiedConsistency() {
+const unifiedConsistency: Plugin = function unifiedConsistency() {
   return (ast, file) => {
     const original = String(file)
     const formatted = String(this.stringify(ast, file))
@@ -23,17 +20,28 @@ const unifiedConsistency: Plugin<[]> = function unifiedConsistency() {
     for (const { deleteText = '', insertText = '', offset, operation } of differences) {
       const toDelete = `\`${showInvisibles(deleteText)}\``
       const toInsert = `\`${showInvisibles(insertText)}\``
-      const position = {
-        start: toPoint(offset),
-        end: toPoint(offset + deleteText.length)
-      }
+      const start = toPoint(offset)!
+      const end = toPoint(offset + deleteText.length)!
+      const position = { start, end }
 
       const message =
         operation === generateDifferences.DELETE
-          ? file.message(`Delete ${toDelete}`, position, del)
+          ? file.message(`Delete ${toDelete}`, {
+              place: position,
+              ruleId: 'delete',
+              source
+            })
           : operation === generateDifferences.REPLACE
-          ? file.message(`Replace ${toDelete} with ${toInsert}`, position, replace)
-          : file.message(`Insert ${toInsert}`, position.start, insert)
+          ? file.message(`Replace ${toDelete} with ${toInsert}`, {
+              place: position,
+              ruleId: 'replace',
+              source
+            })
+          : file.message(`Insert ${toInsert}`, {
+              place: start,
+              ruleId: 'insert',
+              source
+            })
 
       message.url = url
       message.expected = [insertText]
